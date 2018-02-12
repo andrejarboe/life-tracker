bdd117e9c553d9b0819c655eb9aced9500af0a76
andrejarboe <andrejarboe@gmail.com>
2018-02-12 01:00:47 -0500

    add bcrypt

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+var bcrypt = require('bcrypt');
+var SALT_WORK_FACTOR = 10;
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -13,5 +15,21 @@ const UserSchema = new Schema({
 });
 
 const User = mongoose.model("User", UserSchema);
+
+UserSchema.pre('save', function(next){
+  const newUser = this;
+  if (!newUser.isModified('password')) return next();
+
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt){
+      if(err) return next(err);
+
+      bcrypt.hash(newUser.password, salt, function(err, hash){
+          if(err) return next(err);
+
+          newUser.password = hash;
+          next();
+      });
+  });
+});
 
 module.exports = User;
